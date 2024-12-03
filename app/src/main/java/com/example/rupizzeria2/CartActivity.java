278abd95fb2b844f28949c100a687c994c2ff25d
Moises <moisesmcespedes@gmail.com>
2024-12-03 18:39:47 -0500

    ...

---
+++ app/src/main/java/com/example/rupizzeria2/CartActivity.java
@@ -0,0 +1,12 @@
+package com.example.rupizzeria2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
--- app/src/main/java/com/example/rupizzeria2/MainActivity.java
+++ app/src/main/java/com/example/rupizzeria2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.rupizzeria2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,58 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+/**
+ * Main Menu Activity class
+ *
+ * @author Moises Cespedes Moreno, Binoy Patel
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /** Go to order activity button */
+    private Button goToOrder;
+
+    /** Go to cart activity button */
+    private Button goToCart;
+
+    /**
+     * OnCreate method
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        createButtonIntent();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    /**
+     * Creates the Intent for the buttons
+     */
+    private void createButtonIntent(){
+        goToOrder = findViewById(R.id.goToOrder);
+        goToCart = findViewById(R.id.goToCart);
+
+        goToOrder.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+            startActivity(intent);
+        });
+
+        goToCart.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, CartActivity.class);
+            startActivity(intent);
+        });
+    }
+
 }
