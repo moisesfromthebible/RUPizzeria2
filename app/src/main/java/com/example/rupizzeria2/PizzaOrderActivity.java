db768bf3fe5e5ed67f965c3c7be6c94235bdbd9e
Binoy Patel <binoyp2003@gmail.com>
2024-12-03 18:05:55 -0500

    order

---
+++ app/src/main/java/com/example/rupizzeria2/PizzaOrderActivity.java
@@ -0,0 +1,16 @@
+package com.example.rupizzeria2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import androidx.recyclerview.widget.RecyclerView;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import java.util.Arrays;
+import java.util.List;
+public class PizzaOrderActivity extends AppCompatActivity{
+
+}
