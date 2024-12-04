c2bb71b7de007f51fcc9112330a65a5f02e33ffb
Binoy Patel <binoyp2003@gmail.com>
2024-12-04 14:54:38 -0500

    order

--- app/src/main/java/com/example/rupizzeria2/CartActivity.java
+++ app/src/main/java/com/example/rupizzeria2/CartActivity.java
@@ -1,6 +1,9 @@
 package com.example.rupizzeria2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CartActivity extends AppCompatActivity {
@@ -8,5 +11,12 @@ public class CartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+    }
+
+    public void goToHome(View view)
+    {
+        Intent intent = new Intent(CartActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
